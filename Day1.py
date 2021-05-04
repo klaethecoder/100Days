6d91daa5642e00bfd07fa46abca79388898b1605
Klae the Coder <30781713+klaethecoder@users.noreply.github.com>
2021-05-04 09:07:20 -0600

    Finished most of the coding exercises and the project for the day.

--- Day1.py
+++ Day1.py
@@ -3,3 +3,16 @@ answer1 = input("What's the name of the city you grew up in?\n")
 answer2 = input("What's your pet's name?\n")
 
 print(f"Your band name could be {answer1} {answer2}")
+
+# Coding Exercise 1.1
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+# Coding Exercise 1.2 (Fixed code)
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Coding Exercise 1.3
+
+print(len(input("What is your name?\n")))
