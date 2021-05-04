855568a92306d9cbe50ee9810465d9f79225b1dc
Klae the Coder <30781713+klaethecoder@users.noreply.github.com>
2021-05-04 09:32:58 -0600

    Completed all exercises and project in Day 1.

--- Day1.py
+++ Day1.py
@@ -1,8 +1,10 @@
+# Day 1 Project: Band Name Generator
 print("Welcome to the Band Name Generator")
-answer1 = input("What's the name of the city you grew up in?\n")
-answer2 = input("What's your pet's name?\n")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
 
-print(f"Your band name could be {answer1} {answer2}")
+print(f"Your band name could be {city_name} {pet_name}")
+######################################################
 
 # Coding Exercise 1.1
 print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
@@ -14,5 +16,24 @@ print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
 # Coding Exercise 1.3
-
 print(len(input("What is your name?\n")))
+
+# Coding Exercise 1.4 Variables
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+temp = a
+a = b
+b = temp
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
