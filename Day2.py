b3d3468eda5c0803ca82a30b30c18c5556b5ae0f
Klae the Coder <30781713+klaethecoder@users.noreply.github.com>
2021-05-10 08:55:05 -0600

    Getting all the exercises done so that I can finish the project up.

--- Day2.py
+++ Day2.py
@@ -3,12 +3,13 @@ import math
 # Day 2 Project
 print("Welcome to the tip calculator")
 total_bill = float(input("What was the total bill?\t"))
-total_people = float(input("How many people to split the bill?\t"))
 tip_percent = float(input("What percentage tip would you like to give 10, 12, or 15?\t"))/100
+tip_amount = total_bill * tip_percent
+total_people = float(input("How many people to split the bill?\t"))
 
-result = (total_bill + (total_bill*tip_percent)) / total_people
+result = (total_bill + tip_amount) / total_people
 
-print(round(result,2))
+print(f"Each Person should pay: {round(result,2)}")
 
 
 # Coding Exercise Day 2.1 
