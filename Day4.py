926892e2f5f3d8aec59286489ce9e2cc22da89de
Klae the Coder <30781713+klaethecoder@users.noreply.github.com>
2021-05-11 16:54:55 -0600

    Started on Day 4 videos and working through the exercises to work up to the Project.

--- Day4.py
+++ Day4.py
@@ -11,3 +11,30 @@ while set(list) != {0,1,2}:
     list.append(num)
 
 print(f"That took {count} iterations")
+
+# Day 4.1 Exercise: Heads or Tails
+
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+num = random.randint(0,1)
+if num ==1:
+  print("It's Heads")
+else:
+  print("It's Tails")
+
+
+# Day 4.2 Exercise: Who is buying lunch?
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+import random as r
+
+length = len(names)
+index = r.randint(0, length-1)
+
+print(f"Looks like {names[index]} is getting the bill today")
+
