f3a87d03a59b85b01141a12460f2c620e97c2305
Ryan Richard <ryanrichard@Ryans-MacBook-Pro.local>
2021-05-23 22:27:33 -0600

    Finished all the coding to make things work. I am working through clearning up the code.

--- Day7.py
+++ Day7.py
@@ -89,10 +89,10 @@ for letter in word:
 # Step 2
 
 word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = r.choice(word_list)
 
 # Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 
 # TODO-1: - Create an empty List called display.
 # For each letter in the chosen_word, add a "_" to 'display'.
@@ -146,3 +146,54 @@ while "_" in display:
             display[position] = letter
 
     print(display)
+
+# This is exercise 7.3
+# Step 4
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = r.choice(word_list)
+word_length = len(chosen_word)
+
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+# Set 'lives' to equal 6.
+
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
+
+# Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+lives = 6
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    # Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    # TODO-2: - If guess is not a letter in the chosen_word,
+    # Then reduce 'lives' by 1.
+    # If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(lives)
+    print(board[lives])
+    if lives == 0:
+        print("You lost!!!")
+        end_of_game = True
